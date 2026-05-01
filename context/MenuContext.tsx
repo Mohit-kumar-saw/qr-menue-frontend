dd177c021b48da2d0c9265f3de4f2f805cf1b9f8
Mohit-kumar-saw <mohitsaw19@gmail.com>
2026-05-01 22:13:29 +0530

    updates

--- context/MenuContext.tsx
+++ context/MenuContext.tsx
@@ -3,7 +3,7 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://qr-menue-backend.onrender.com/api";
 
 interface MenuContextType {
   categories: any[];
